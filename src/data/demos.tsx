ad8bc98773b8a8a33e79650bb32f1aad74baed7c
Melory Ayala <melory.ayala@gmail.com>
2023-08-31 11:56:48 +0200

    Creating fake data for demos

---
+++ src/data/demos.tsx
@@ -0,0 +1,59 @@
+const lastDemos = [
+    {
+        productDemo: 'Pro suit XS',
+        demoComment: 'Comment regarding the demo or client need.',
+        demoStatus: 'Open demo',
+        dateHistoric: {
+            openDemo: '20/02/2023',
+            sendArticle: '',
+            completedDemo: '',
+        },
+        clientInfo: {
+            pronoun: 'Madam',
+            name: 'Silvia Balman',
+            streetAdress: 'Dornacherstrasse',
+            numberAdress: 6,
+            cityAdress: 'Zurich',
+            postNumberAdress: 6003,
+        },
+    },
+    {
+        productDemo: 'Pro suit XL',
+        demoComment: '',
+        demoStatus: 'Send article',
+        dateHistoric: {
+            openDemo: '15/04/2023',
+            sendArticle: '20/06/2023',
+            completedDemo: '',
+        },
+        clientInfo: {
+            pronoun: 'Sir',
+            name: 'Paul Müller',
+            streetAdress: 'Helvetiastrasse',
+            numberAdress: 23,
+            cityAdress: 'Bern',
+            postNumberAdress: 3005,
+        },
+
+    },
+    {
+        productDemo: 'Home suit M',
+        demoComment: '',
+        demoStatus: 'Completed',
+        dateHistoric: {
+            openDemo: '10/01/2023',
+            sendArticle: '15/02/2023',
+            completedDemo: '20/02/2023',
+        },
+        clientInfo: {
+            pronoun: 'Madam',
+            name: 'Maria Fall',
+            streetAdress: 'Dornacherstrasse',
+            numberAdress: 6,
+            cityAdress: 'Luzern',
+            postNumberAdress: 6003,
+        },
+    },
+]
+
+export default lastDemos;
